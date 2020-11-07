f773f9b9b30be554e6d8c8242a9ff1f856d63acb
Lee Bryant <contact@leecbryant.com>
2020-11-06 20:09:00 -0800

    test for quick sell

--- tester.py
+++ tester.py
@@ -1,6 +1,6 @@
 from wizAPI import *
+import time
 
 tester = wizAPI().register_window(nth=0)
 
-test_pos = tester.get_friendly_pos('star.png')
-print('Friend at pos', test_pos)
+test_pos = tester.quick_sell(False, False)
