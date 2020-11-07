32a47c51470af49c1ea2adb1e803245c76605575
Lee Bryant <contact@leecbryant.com>
2020-11-06 22:51:00 -0800

    string testing

--- tester.py
+++ tester.py
@@ -1,6 +1,12 @@
 from wizAPI import *
 import time
 
-tester = wizAPI().register_window(nth=0)
+# tester = wizAPI().register_window(nth=0)
 
-test_pos = tester.quick_sell(False, False)
+# test_pos = tester.quick_sell(False, False)
+
+test_str = "friend_test.png"
+
+test_str = test_str.split('.')[0].split('_')[1]
+
+print(test_str)
