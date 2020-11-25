9e0326c3764d4fb42d076cb6e1338242beb1108a
Lee Bryant <contact@leecbryant.com>
2020-11-24 20:27:54 -0800

    func rename

--- wizAPI.py
+++ wizAPI.py
@@ -790,8 +790,8 @@ class wizAPI:
             else:
                 self.pass_turn()
 
-    #Catacombs Extract Random attack sequence
-    def catacombs_attack(self, wizard_type, boss_pos):
+    #One round strat for most mobs
+    def mass_feint_strategy(self, wizard_type, boss_pos):
         wizard_type = wizard_type.split('.')[0]
 
         print(wizard_type)
