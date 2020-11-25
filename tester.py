76a199e94bcd98f90cead268f26ee869ea31c4c1
Lee Bryant <contact@leecbryant.com>
2020-11-24 21:46:46 -0800

    new test for multithreaded

--- tester.py
+++ tester.py
@@ -8,42 +8,11 @@ while True:
 
     threads = []
 
-    t = Thread(target=tester.hold_key, args=('w', 30.498, 0))
+    t = Thread(target=tester.hold_key, args=('w', 13.752, 0))
     threads.append(t)
 
-    t = Thread(target=tester.hold_key, args=('d', 0.451, 3.619))
+    t = Thread(target=tester.navigate_keys, args=(['a','a','a','d','a','a','a','a','a','a','a','d'],[0.273,0.454,0.26,0.164,0.304,0.319,0.185,0.374,0.292,0.184,0.1,0.094],[1.195,2.013,2.931,4.18,5.501,6.723,7.494,9.511,10.747,11.707,12.259,12.689]))
     threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('d', 0.376, 8.204))
-    threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('d', 0.395, 11.282))
-    threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('d', 0.127, 12.997))
-    threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('d', 0.035, 18.203))
-    threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('d', 0.032, 18.32))
-    threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('d', 0.592, 22.078))
-    threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('d', 0.427, 24.991))
-    threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('a', 0.147, 28.8))
-    threads.append(t)
-
-    t = Thread(target=tester.hold_key, args=('d', 0.211, 30.072))
-    threads.append(t)
-
-    
-
-    
 
     
 
