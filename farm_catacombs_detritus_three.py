2c69ac9aec998738d8d539cfff806dc8298f5190
Lee Bryant <contact@leecbryant.com>
2020-12-01 15:22:47 -0800

    loading fix

--- farm_catacombs_detritus_three.py
+++ farm_catacombs_detritus_three.py
@@ -26,9 +26,8 @@ if(blader.get_window_rect()[0] > feinter.get_window_rect()[0]):
 
 def await_finished_loading(windows):
     for w in windows:
-        while not w.is_logo_bottom_right_loading() or is_logo_bottom_left_loading(): # Looks at either side for logo - update for wiz loading screen update
+        while not w.is_logo_bottom_right_loading() or w.is_logo_bottom_left_loading(): # Looks at either side for logo - update for wiz loading screen update
             time.sleep(.2)
-            print("Waiting on Load")
 
     for w in windows:
         while not w.is_idle():
