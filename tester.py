be9755a7fdc9f3632c197a90126fa9570e970b97
Lee Bryant <contact@leecbryant.com>
2020-12-05 18:17:37 -0800

    extra test bed functions

--- tester.py
+++ tester.py
@@ -2,9 +2,10 @@ from wizAPI import *
 import time
 from threading import Thread
 
-tester = wizAPI().register_window(nth=1)
+tester = wizAPI().register_window(nth=2)
 wx, wy = tester.get_window_rect()[:2]
 
 #tester.use_potion_if_needed(health_percent=80)
  #108, 551
-tester.quick_sell(False,False)
+# tester.screenshot('test.png', (665, 325, 40, 70))
+tester.bazaar_sell("feinter.png")
